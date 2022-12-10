625b3991c19d86afc9a5c122670533641f7dc215
Josh <joshuagarza42@gmail.com>
2022-12-10 12:39:30 -0800

    adding major airports

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,6 +4,16 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// Get data from cities.js (Pulling from separate java file)
+let cityData = cities;
+
+// Loop through the aray of cities and create one marker per city
+cities.forEach(city => {
+    console.log(city.location)
+    L.marker(city.location).addTo(map)
+});
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,7 +21,5 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
